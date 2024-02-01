bda97c1348c71547644fc611983c3d5c8057f086
yonainthefish <yonaisgood@gmail.com>
2024-02-02 00:26:56 +0900

    :bulb: Feat: 로그아웃 기능 구현(#11)

--- src/components/common/NavBar.tsx
+++ src/components/common/NavBar.tsx
@@ -1,29 +1,31 @@
 import React from 'react';
 
+import { logout } from '../../utils/SDKUtils';
+
 import { Link } from 'react-router-dom';
-import search from '@/assets/Icon/Icon-search.svg';
-import home from '@/assets/Icon/Icon-home.svg';
-import upload from '@/assets/Icon/Icon-upload.svg';
-import mypage from '@/assets/Icon/Icon-my.svg';
-import logout from '@/assets/Icon/Icon-logout.svg';
+import searchIcon from '@/assets/Icon/Icon-search.svg';
+import homeIcon from '@/assets/Icon/Icon-home.svg';
+import uploadIcon from '@/assets/Icon/Icon-upload.svg';
+import mypageIcon from '@/assets/Icon/Icon-my.svg';
+import logoutIcon from '@/assets/Icon/Icon-logout.svg';
 
 const menuItems = [
-  { to: '/search', label: 'Search', image: search },
-  { to: '/home', label: 'Home', image: home },
-  { to: '/upload', label: 'Upload', image: upload },
-  { to: '/mypage', label: 'My', image: mypage },
-  { to: '/logout', label: 'Logout', image: logout },
+  { to: '/search', label: 'Search', image: searchIcon },
+  { to: '/home', label: 'Home', image: homeIcon },
+  { to: '/upload', label: 'Upload', image: uploadIcon },
+  { to: '/mypage', label: 'My', image: mypageIcon },
+  { to: '/login', label: 'Logout', image: logoutIcon },
 ];
 
 export default function Navbar() {
   const commonClasses =
-    'bg-gray-0 w-full h-20  text-gray-600 text-small flex justify-center items-center hover:bg-primary-30';
+    'bg-gray-0 w-full h-20 text-gray-600 text-small flex justify-center items-center hover:bg-primary-30';
 
   return (
-    <nav className="bg-gray-0 h-full w-20 border-l-2 border-gray-100 fixed right-0 z-10 flex flex-col">
+    <nav className="bg-gray-0 h-full w-20 border-l-2 border-gray-100 fixed right-0 flex flex-col z-30">
       {menuItems.map((item, index) => (
         <Link key={index} to={item.to} className={commonClasses}>
-          <div>
+          <div onClick={item.label === 'Logout' ? logout : undefined}>
             <img src={item.image} alt={item.label} className="mx-auto w-5" />
             <p>{item.label}</p>
           </div>
