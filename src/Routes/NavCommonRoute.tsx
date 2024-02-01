a0aa2061d15afa9e265bf6a1ebf7e9910f6bc791
yonainthefish <yonaisgood@gmail.com>
2024-02-01 13:25:08 +0900

    :bulb: Feat: 사용자 정보 확인시 접근 가능한 페이지에서 공통으로 사용하기 위해 commonRoute 생성(#4)

---
+++ src/Routes/NavCommonRoute.tsx
@@ -0,0 +1,16 @@
+import { Outlet } from 'react-router-dom';
+
+import NavBar from '@/components/common/NavBar';
+import Header from '@/components/common/Header';
+import Footer from '@/components/common/Footer';
+
+export default function NavCommonRoute() {
+  return (
+    <>
+      <Header />
+      <NavBar />
+      <Footer />
+      <Outlet />
+    </>
+  );
+}
