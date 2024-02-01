f5fe4d85a37150855ab58e0c9bd0f8715e384ec0
yonainthefish <yonaisgood@gmail.com>
2024-02-01 13:19:15 +0900

    :bulb: Feat: useLogin 훅 사용하여 로그인 기능 구현

--- src/pages/LogIn.tsx
+++ src/pages/LogIn.tsx
@@ -1,5 +1,9 @@
-import React from 'react';
+import { ChangeEvent, FormEvent, useEffect, useState } from 'react';
+// import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+
+import { useLogin } from '../hook/useLogin';
+
 import { Button } from '../components/ui/Button';
 import { Input } from '../components/ui/Input';
 import { Label } from '../components/ui/Label';
@@ -9,11 +13,89 @@ import LogoText from '../assets/Img/Logo-text-small.svg';
 import googleIcon from '../assets/Img/Img-google.svg';
 import githubIcon from '../assets/Img/Img-github.svg';
 import kakaoIcon from '../assets/Img/Img-kakao.svg';
+import LoadingIcon from '@/assets/Icon/Icon-loading-black.svg';
 
 export default function LogIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailValid, setEmailValid] = useState(false);
+  const [passwordValid, setPasswordValid] = useState(false);
+  const [emailErrMessage, setEmailErrMessage] = useState('');
+  const [passwordErrMessage, setPasswordErrMessage] = useState('');
+  const [loginErrMessage, setLoginErrMessage] = useState('');
   const gradientStyle = {
     backgroundImage:
       'linear-gradient(113.78deg, rgba(250, 218, 128, 0.4) 21.18%, rgba(243, 117, 37, 0.4) 55.65%, rgba(237, 67, 35, 0.4) 82.73%)',
+  };
+
+  const { login, error, isPending } = useLogin();
+  
+
+  useEffect(() => {
+    if (error === null) {
+  
+      return;
+    }
+
+    switch (error) {
+      case 'auth/invalid-login-credentials':
+        setLoginErrMessage('아이디 혹은 비밀번호가 일치하지 않습니다');
+        break;
+      case 'auth/user-not-found':
+        setLoginErrMessage('존재하지 않는 계정입니다');
+        break;
+      case 'auth/wrong-password':
+        setPasswordErrMessage('비밀번호를 다시 확인해주세요');
+        setPasswordValid(false);
+        break;
+      case 'auth/network-request-failed':
+        alert('네트워크 연결에 실패했습니다');
+        break;
+      case 'auth/internal-error':
+        alert('잘못된 요청입니다');
+        break;
+      case 'auth/too-many-requests':
+        setLoginErrMessage(
+          '여러 번 로그인에 실패하여 해당 계정에 대한 로그인이 비활성화되었습니다. 나중에 다시 시도해 주세요',
+        );
+        break;
+      default:
+        setLoginErrMessage('아이디 혹은 비밀번호를 확인해주세요');
+    }
+  }, [error]);
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    login(email, password);
+  };
+
+  const handleEmailInp = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+    setLoginErrMessage('');
+
+    if (e.target.validity.valueMissing) {
+      setEmailValid(false);
+      setEmailErrMessage('이메일을 입력해주세요');
+    } else if (e.target.validity.typeMismatch) {
+      setEmailValid(false);
+      setEmailErrMessage('이메일 형식이 올바르지 않습니다');
+    } else {
+      setEmailValid(true);
+      setEmailErrMessage('');
+    }
+  };
+
+  const handlePasswordInp = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+    setLoginErrMessage('');
+
+    if (e.target.validity.valueMissing) {
+      setPasswordValid(false);
+      setPasswordErrMessage('비밀번호를 입력해주세요');
+    } else {
+      setPasswordValid(true);
+      setPasswordErrMessage('');
+    }
   };
 
   return (
@@ -31,11 +113,29 @@ export default function LogIn() {
           <img src={LogoText} alt="바이블브릿지 텍스트 로고" />
         </h1>
 
-        <form className="w-[100%]">
+        <form className="w-[100%]" onSubmit={handleSubmit}>
           <Label htmlFor="email-input" className="sr-only">
             이메일
           </Label>
-          <Input type="email" id="email-input" placeholder="이메일" required />
+          <Input
+            type="email"
+            id="email-input"
+            placeholder="이메일"
+            required
+            onChange={handleEmailInp}
+          />
+          <div style={{ position: 'relative' }}>
+            <strong
+              role="alert"
+              className={`text-error text-smaller   ${
+                emailErrMessage ? 'visible' : 'invisible'
+              }`}
+              style={{ position: 'absolute', left: '10px', top: '-17px' }}
+            >
+              {emailErrMessage && `*${emailErrMessage}`}
+            </strong>
+          </div>
+
           <Label htmlFor="password-input" className="sr-only">
             비밀번호
           </Label>
@@ -44,12 +144,35 @@ export default function LogIn() {
             id="password-input"
             placeholder="비밀번호"
             required
+            onChange={handlePasswordInp}
           />
+          <div>
+            <strong
+              role="alert"
+              className={`text-error text-smaller
+              ${
+                passwordErrMessage || loginErrMessage ? 'visible' : 'invisible'
+              }`}
+            >
+              {passwordErrMessage
+                ? `*${passwordErrMessage}`
+                : loginErrMessage
+                ? `*${loginErrMessage}`
+                : ''}
+            </strong>
+          </div>
+          <Button
+            className={`${
+              !emailValid || !passwordValid || isPending
+                ? 'opacity-30 cursor-not-allowed'
+                : ''
+            }`}
+          >
+            {isPending ? <img src={LoadingIcon} alt="로그인 중" /> : '로그인'}
+          </Button>
         </form>
 
-        <Button type="submit">로그인</Button>
-
-        <p className="w-[100%] mt-12 text-basic text-gray-600 bg-gray-basic-700">
+        <p className="w-[100%] mt-12 text-small text-gray-600 bg-gray-basic-700">
           간편 로그인
         </p>
         <div className="flex gap-5 m-5">
