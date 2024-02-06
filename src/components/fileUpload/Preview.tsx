f2b919dd8899942f650f95e31782f6649e22c72e
yonainthefish <yonaisgood@gmail.com>
2024-02-06 10:43:34 +0900

    :bulb: Feat: 업로드시 이미지 그려줄 preview 컴포넌트 생성 (#18)

---
+++ src/components/fileUpload/Preview.tsx
@@ -0,0 +1,71 @@
+import { useState, SetStateAction, Dispatch, useEffect } from 'react';
+import ImgUpload from '@/assets/Icon/Icon-add.svg';
+
+const Preview = ({
+  setFile,
+  imgUrlList,
+}: {
+  setFile: Dispatch<SetStateAction<FileList | null>>;
+  imgUrlList: string[];
+}) => {
+  const [imageUrl, setImageUrl] = useState<string>('');
+
+  const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+    if (files && files.length > 0) {
+      const file = files[0];
+      setFile(files);
+
+      if (!/^image\/(jpg|svg|png|jpeg|bmp|tif|heic)$/.test(file.type)) {
+        alert(
+          '이미지 파일 확장자는 jpg, svg, png, jpeg, bmp, tif, heic만 가능합니다.',
+        );
+        return;
+      }
+
+      if (file.size > 10 * 1024 * 1024) {
+        alert('이미지 용량은 10MB 이내로 등록 가능합니다.');
+
+        setFile(null);
+        setImageUrl('');
+        return;
+      }
+
+      const imageUrl = URL.createObjectURL(file);
+      setImageUrl(imageUrl);
+    } else {
+      alert('이미지 파일을 선택해주세요.');
+      setFile(null);
+      setImageUrl('');
+    }
+  };
+
+  useEffect(() => {
+    setImageUrl(imgUrlList[0] || '');
+  }, [imgUrlList]);
+
+  return (
+    <>
+      <label htmlFor="file">
+        <div className="btnUpload">
+          <img src={ImgUpload} alt="사진 업로드 버튼" />
+        </div>
+      </label>
+      <input
+        accept="image/*"
+        type="file"
+        id="file"
+        onChange={(e) => handleImageUpload(e)}
+      />
+
+      {/* 업로드된 이미지 미리보기 */}
+      {imageUrl && <img src={imageUrl} alt="업로드된 이미지 미리보기" />}
+    </>
+  );
+};
+
+Preview.defaultProps = {
+  imgUrlList: [],
+};
+
+export default Preview;
