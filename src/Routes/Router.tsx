1b095dc8338ce1d008eeafd8c8c53cefa6d1c71f
yonainthefish <yonaisgood@gmail.com>
2024-02-06 10:39:22 +0900

    :writing_hand: Rename: import 절대경로 수정, route페이지 추가 (#4)

--- src/Routes/Router.tsx
+++ src/Routes/Router.tsx
@@ -1,15 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { AuthRoute, NonAuthRoute } from './AuthRoute';
-import useAuthContext from '../hook/useAuthContext';
-import NavCommonRoute from '@/Routes/LayoutRoute';
+import { AuthRoute, NonAuthRoute } from '@/routes/AuthRoute';
+import useAuthContext from '@/hook/useAuthContext';
+import NavCommonRoute from '@/routes/LayoutRoute';
 
 import Splash from '@/pages/Splaysh/Splash';
 import StaticSplash from '@/pages/Splaysh/StaticSplash';
 
-import LogIn from '@/pages/LogIn';
+import LogIn from '@/pages/Login/LogIn';
 import SignUp from '@/pages/SignUp';
 import Home from '@/pages/Home';
 import MyPage from '@/pages/Mypage/MyPage';
+import Setting from '@/pages/Setting';
+import Upload from '@/components/uploadUI/Upload';
 
 export default function Router() {
   const { isAuthReady } = useAuthContext();
@@ -32,6 +34,8 @@ export default function Router() {
                 <Route element={<NavCommonRoute />}>
                   <Route path="/home" element={<Home />} />
                   <Route path="/mypage" element={<MyPage />} />
+                  <Route path="/upload" element={<Upload />} />
+                  <Route path="/setting" element={<Setting />}></Route>
                 </Route>
               </Route>
             </>
