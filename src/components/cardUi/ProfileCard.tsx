73fde619b253c47198bce5e16be5517bc4ccb71d
yonainthefish <yonaisgood@gmail.com>
2024-02-08 18:04:33 +0900

    :bug: Fix: 프로필카드에서 소개내용 오지 않던 오류 수정(#17)

--- src/components/cardUi/ProfileCard.tsx
+++ src/components/cardUi/ProfileCard.tsx
@@ -1,23 +1,45 @@
-// import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { Button } from '@/components/ui/Button';
-import UserProfileImg from '@/assets/Img/Img-user.svg';
+import { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
+import { doc, getDoc, DocumentData } from 'firebase/firestore';
+import { appFireStore } from '@/firebase/config';
+
 import useAuthContext from '@/hook/useAuthContext';
-import { useNavigate } from 'react-router-dom';
+
+import { Button } from '@/components/ui/Button';
+
+import UserProfileImg from '@/assets/Img/Img-user.svg';
 
 export default function ProfileCard() {
   const { user } = useAuthContext();
+  const [userDetails, setUserDetails] = useState<DocumentData | null>(null);
+
   const navigate = useNavigate();
   console.log(user);
-
-  if (!user) {
-    navigate('/login');
-    return;
-  }
 
   const handleEditProfile = () => {
     navigate('/setting');
   };
+
+  useEffect(() => {
+    const fetchUserDetails = async () => {
+      if (user) {
+        const userRef = doc(appFireStore, 'users', user.uid);
+        const docSnap = await getDoc(userRef);
+        if (docSnap.exists()) {
+          setUserDetails(docSnap.data());
+        } else {
+          console.log('No such document!');
+        }
+      }
+    };
+
+    fetchUserDetails();
+  }, [user]);
+
+  if (!user || !userDetails) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <article className="w-[380px] h-[800px] px-[35px] py-[40px] rounded-md bg-gray-0 border-2 border-gray-200">
@@ -25,7 +47,7 @@ export default function ProfileCard() {
         <img
           src={user.photoURL || UserProfileImg}
           alt="프로필 사진"
-          className="mx-auto mb-6"
+          className="w-[200px] mx-auto mb-6 rounded-full aspect-square object-cover"
         />
         <div className="text-gray-700 text-nameSize font-bold">
           {user.displayName == null
@@ -34,8 +56,7 @@ export default function ProfileCard() {
         </div>
         <div className="text-gray-600">{user.email}</div>
         <div className="max-h-20 my-4 line-clamp-3">
-          {/* {user.introduce} */}
-          소개글
+          <div>{userDetails.introduce || '소개글이 설정되지 않았습니다.'}</div>
         </div>
         <Link to="/setting">
           <Button className="w-full" onClick={handleEditProfile}>
