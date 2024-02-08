726aa648d907231d722f28936fd92e3f597b6899
yonainthefish <yonaisgood@gmail.com>
2024-02-08 18:48:12 +0900

    :art: Design: 오버레이 컴포넌트 생성

---
+++ src/components/ui/Overlay.tsx
@@ -0,0 +1,22 @@
+interface OverlayProps {
+  opacity?: number;
+  backgroundColor?: string;
+}
+
+const Overlay: React.FC<OverlayProps> = ({
+  opacity = 50,
+  backgroundColor = 'black',
+}) => {
+  const opacityValue = opacity / 100;
+  return (
+    <div
+      className={`absolute top-0 left-0 right-0 bottom-0`}
+      style={{
+        backgroundColor,
+        opacity: opacityValue,
+      }}
+    ></div>
+  );
+};
+
+export default Overlay;
