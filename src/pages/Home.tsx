3dab390f80db05399cf024a0578c1a769ac0a8e9
yonainthefish <yonaisgood@gmail.com>
2024-02-08 18:44:28 +0900

    :art: Design: 메인 페이지 기본 레이아웃 구성 수정(#21)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
-
+import { Calendar } from '@/components/ui/Calendar';
+import FeedUi from '@/components/cardUi/FeedItemCard';
 export default function Home() {
+  const [date, setDate] = React.useState<Date | undefined>(new Date());
+
   return (
-    <>
-      <p>Home</p>
-    </>
+    <main className="grid grid-cols-2 gap-4 mt-[60px] ">
+      <section className="border-gray-700 border-2 bg-gray-500">
+        <Calendar
+          mode="single"
+          selected={date}
+          onSelect={setDate}
+          className="rounded-md border w-full"
+        />
+      </section>
+      <section className="bg-gray-600">
+        <FeedUi />
+      </section>
+    </main>
   );
 }
