a058aa86885c92f5dd3b2ac3efef626cf9c711b0
yonainthefish <yonaisgood@gmail.com>
2024-02-08 18:47:00 +0900

    :bulb: Feat: 게시글 수정, 삭제를 위한 contextProvider 추가

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,12 @@ import App from './App.tsx';
 import './index.css';
 
 import { AuthContextProvider } from '@/context/AuthContext.tsx';
+import EditContextProvider from '@/context/EditContext.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <AuthContextProvider>
+  <AuthContextProvider>
+    <EditContextProvider>
       <App />
-    </AuthContextProvider>
-  </React.StrictMode>,
+    </EditContextProvider>
+  </AuthContextProvider>,
 );
