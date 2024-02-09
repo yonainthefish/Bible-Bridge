238e09b9436c80b1c64aed5b30be33fe4094708a
yonainthefish <yonaisgood@gmail.com>
2024-02-09 10:52:23 +0900

    :art: Design: 메인페이지 퍼블리싱 추가(#21)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,28 @@
 import React from 'react';
 import { Calendar } from '@/components/ui/Calendar';
 import FeedUi from '@/components/cardUi/FeedItemCard';
+import TopButton from '@/components/ui/TopButton/TopButton';
 export default function Home() {
   const [date, setDate] = React.useState<Date | undefined>(new Date());
 
   return (
-    <main className="grid grid-cols-2 gap-4 mt-[60px] ">
-      <section className="border-gray-700 border-2 bg-gray-500">
-        <Calendar
-          mode="single"
-          selected={date}
-          onSelect={setDate}
-          className="rounded-md border w-full"
-        />
-      </section>
-      <section className="bg-gray-600">
-        <FeedUi />
-      </section>
-    </main>
+    <div className="mx-16 relative">
+      <main className="flex">
+        <section className="w-[50%] h-[100vh] bg-gray-100 fixed">
+          <Calendar
+            mode="single"
+            selected={date}
+            onSelect={setDate}
+            className="w-[50%] rounded-md mx-auto"
+          />
+        </section>
+        <section className="w-[50%] absolute top-0 right-0  bg-gray-300">
+          <div className="w-[300px] px-[20px] bg-gray-700 mx-auto">
+            <FeedUi />
+          </div>
+        </section>
+      </main>
+      <TopButton />
+    </div>
   );
 }
