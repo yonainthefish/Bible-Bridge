acc555922e7eac75b6d5642e0ea9a83f8cbab09f
yonainthefish <yonaisgood@gmail.com>
2024-02-09 10:54:11 +0900

    :art: Design: 전체화면 레이아웃 위치 조정(#6)

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '@/assets/Img/Logo-text-small.svg';
 
 export default function Header() {
   return (
     <>
-      <header className="w-full h-20 px-16 bg-gray-0 flex items-center fixed top-0 left-0 right-0 z-20">
+      <header className="w-[calc(100%-80px)] h-20 px-16 bg-gray-0 flex items-center fixed top-0 left-0 right-0 z-20">
         <Link to="/home">
           <h1>
             <img className="w-[250px]" src={Logo} alt="바이블브릿지 로고" />
--- src/components/common/NavBar.tsx
+++ src/components/common/NavBar.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
-
-import { logout } from '../../utils/SDKUtils';
-
 import { Link } from 'react-router-dom';
+import { logout } from '@/utils/SDKUtils';
+
 import searchIcon from '@/assets/Icon/Icon-search.svg';
 import homeIcon from '@/assets/Icon/Icon-home.svg';
 import uploadIcon from '@/assets/Icon/Icon-upload.svg';
-import mypageIcon from '@/assets/Icon/Icon-my.svg';
+import myPageIcon from '@/assets/Icon/Icon-my.svg';
 import logoutIcon from '@/assets/Icon/Icon-logout.svg';
 import exploreIcon from '@/assets/Icon/Icon-explore.svg';
 
@@ -15,20 +13,27 @@ const menuItems = [
   { to: '/search', label: 'Search', image: searchIcon },
   { to: '/explore', label: 'Explore', image: exploreIcon },
   { to: '/upload', label: 'Upload', image: uploadIcon },
-  { to: '/mypage', label: 'My', image: mypageIcon },
+  { to: '/myPage', label: 'My', image: myPageIcon },
   { to: '/login', label: 'Logout', image: logoutIcon },
 ];
 
 export default function Navbar() {
   const commonClasses =
-    'bg-gray-0 w-full h-20 text-gray-600 text-small flex justify-center items-center hover:bg-primary-30';
+    'w-full h-20 bg-gray-0 text-gray-600 text-small flex justify-center items-center hover:bg-primary-30';
+
+  const navStyle =
+    'w-20 h-full bg-gray-0 border-l-2 border-gray-100 fixed top-0 right-0 flex flex-col z-30';
 
   return (
-    <nav className="bg-gray-0 h-full w-20 border-l-2 border-gray-100 fixed right-0 flex flex-col z-30">
+    <nav className={navStyle}>
       {menuItems.map((item, index) => (
         <Link key={index} to={item.to} className={commonClasses}>
           <div onClick={item.label === 'Logout' ? logout : undefined}>
-            <img src={item.image} alt={item.label} className="mx-auto w-5" />
+            <img
+              src={item.image}
+              alt={item.label}
+              className="w-[20px] mx-auto"
+            />
             <p>{item.label}</p>
           </div>
         </Link>
