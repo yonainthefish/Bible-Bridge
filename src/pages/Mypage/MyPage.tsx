aaabeae9c8f909c03784a133d7e6128f86026da8
yonainthefish <yonaisgood@gmail.com>
2024-02-09 10:51:47 +0900

    :art: Design: 마이페이지 구성, CSS 수정(#14)

--- src/pages/Mypage/MyPage.tsx
+++ src/pages/Mypage/MyPage.tsx
@@ -1,54 +1,21 @@
-import { useState } from 'react';
-
 import ProfileCard from '@/components/cardUi/ProfileCard';
-import './MyPage.css';
-import FeedUi from '@/components/cardUi/FeedUi';
-import { Calendar } from '@/components/ui/Calendar';
+import FeedItemCard from '@/components/cardUi/FeedItemCard';
+import '@/pages/myPage/myPage.css';
 
-type ContentType = 'calendar' | 'meditation';
-
-interface ButtonInfo {
-  type: ContentType;
-  label: string;
-}
-
-export default function MyPage() {
-  const [selectedContent, setSelectedContent] =
-    useState<ContentType>('calendar');
-  const buttons: ButtonInfo[] = [
-    { type: 'calendar', label: '달력' },
-    { type: 'meditation', label: '전체묵상' },
-  ];
-
-  const handleBtnClick = (content: ContentType) => {
-    setSelectedContent(content);
-  };
-
+export default function myPage() {
   return (
     <main className="h-[100vh] ">
-      <div className="before-element z-0"></div>
-      <div className="flex pl-[70px] pr-[150px] pt-[100px] gap-10 z-10">
+      <div className="beforeElement z-0"></div>
+      <div className="flex px-[70px] pt-[100px] gap-10 z-10">
         <section className="relative">
           <ProfileCard />
         </section>
-        <section className="w-[1000px] h-[1000px] bg-gray-400 relative">
-          <div className="mt-[180px] text-left">
-            {buttons.map((button) => (
-              <button
-                key={button.type}
-                className={`mx-4 ${
-                  selectedContent === button.type ? 'font-bold' : ''
-                }`}
-                onClick={() => handleBtnClick(button.type)}
-              >
-                {button.label}
-              </button>
-            ))}
+        <section className="w-full relative">
+          <div className="mt-[80px] mb-[30px] text-left text-gray-900 relative">
+            전체묵상
+            <div className="bottomLine"></div>
           </div>
-          <div className="bg-gray-100 mt-5">
-            {selectedContent === 'calendar' ? <Calendar /> : null}
-            {selectedContent === 'meditation' ? <FeedUi /> : null}
-          </div>
+          <FeedItemCard />
         </section>
       </div>
     </main>
