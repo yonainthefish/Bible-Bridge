b114c36e42982392e96fb4b991a458648ecbce26
yonainthefish <yonaisgood@gmail.com>
2024-02-16 22:55:06 +0900

    :lipstick: Style: model은 import시 맨 위 위치하도록 순서 변경(#33)

--- src/pages/feedDetail/FeedDetail.tsx
+++ src/pages/feedDetail/FeedDetail.tsx
@@ -3,9 +3,9 @@ import { useParams } from 'react-router-dom';
 import { appFireStore } from '@/firebase/config';
 import { doc, getDoc } from 'firebase/firestore';
 
+import { FeedItem as FeedItemType } from '@/components/cardUi/model';
 import Comment from '@/components/userReactionUi/commentUi/Comment';
 import CommentsList from '@/components/userReactionUi/commentUi/CommentList';
-import { FeedItem as FeedItemType } from '@/components/cardUi/model';
 import FeedInfo from '@/components/cardUi/FeedInfo';
 
 export default function FeedDetail() {
@@ -34,10 +34,14 @@ export default function FeedDetail() {
   }
 
   return (
-    <>
-      {feed && <FeedInfo feed={feed} />}
-      {id && <Comment postId={id} />}
-      {id && <CommentsList postId={id} />}
-    </>
+    <section className="w-[70%] h-[80vh] mx-auto flex border-2 border-gray-700">
+      <div className="flex-1">{feed && <FeedInfo feed={feed} />}</div>
+      <div className="flex-1">
+        <div className="h-[calc(100%-40px)] border-2 border-gray-300 overflow-y-auto">
+          {id && <CommentsList postId={id} />}
+        </div>
+        {id && <Comment postId={id} />}
+      </div>
+    </section>
   );
 }
