c271eecf0b30f96be7aab380a20d2b0d47b8782c
yonainthefish <yonaisgood@gmail.com>
2024-02-17 16:24:01 +0900

    :recycle: Refactor: 훅에서 사용하는 interface 모듈 제네릭 사용하여 중복제거

--- src/hook/model.ts
+++ src/hook/model.ts
@@ -1,10 +1,10 @@
 interface UserFormData<T> {
-  email: T;
-  password: T;
-  displayName: T;
-  file: File | T;
-  introduce?: string | T;
+  email: T | null;
+  password: T | null;
+  displayName: T | null;
+  file: File | null;
+  introduce?: string | T | null;
 }
 
 export type SignUpProps = UserFormData<string | null>;
-export type Profile = UserFormData<null>;
+export type ProfileProps = UserFormData<string>;
