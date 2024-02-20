d27561aeb2b9c4bc89931b06a83b4d255cf9714d
yonainthefish <yonaisgood@gmail.com>
2024-02-20 14:36:53 +0900

    :art: Design: 업로드모달 퍼블리싱(#18)

--- src/pages/upload/Upload.tsx
+++ src/pages/upload/Upload.tsx
@@ -5,13 +5,13 @@ import { doc, setDoc } from 'firebase/firestore';
 import { v4 as uuidv4 } from 'uuid';
 
 import Preview from '@/components/fileUpload/Preview';
-import { Label } from '@/components/commonUi/label/Label';
 import { Input } from '@/components/commonUi/input/Input';
 import { Button } from '@/components/commonUi/button/Button';
 
 import useAuthContext from '@/hook/useAuthContext';
 import useUploadContext from '@/hook/useUploadContext';
 import uploadImageToStorage from '@/pages/upload/UploadImgToStorage';
+import closeImg from '@/assets/Icon/Icon-close.svg';
 
 export default function Upload() {
   const navigate = useNavigate();
@@ -60,7 +60,6 @@ export default function Upload() {
     if (dialogRef.current && dialogRef.current.showModal) {
       dialogRef.current.showModal();
     }
-    console.log('dkdk');
   }, []);
 
   const closeUploadModal = () => {
@@ -71,40 +70,50 @@ export default function Upload() {
   };
 
   return (
-    <dialog ref={dialogRef} aria-labelledby="dialog-label">
-      <h2>Upload Post</h2>
-      <p>오늘의 묵상을 나눠주세요</p>
-      <div className=" w-[100px] h-[100px]  mx-auto ">
+    <dialog
+      ref={dialogRef}
+      aria-labelledby="dialog-label"
+      className="w-[32%] py-[25px] px-[70px] relative rounded-sm backdrop-blur-3xl"
+    >
+      <h2 className="text-title font-700">Upload Post</h2>
+      <p className="text-small text-gray-500">오늘의 묵상을 나눠주세요</p>
+      <div className=" w-[100%] aspect-square  mx-auto my-[15px] bg-gray-900 flex items-center justify-center">
         <Preview setFile={setFile} />
       </div>
-      <div>
-        <Label></Label>
-        <Input
-          type="text"
-          placeholder="제목"
-          value={title}
+      <Input
+        type="text"
+        placeholder="묵상한 구절을 적어주세요"
+        value={title}
+        onChange={(e) => {
+          setTitle(e.target.value);
+        }}
+        className="mt-[25px] mb-[10px] border-2 border-gray-300 rounded-sm"
+      ></Input>
+      <form className="uploadInfo">
+        <textarea
+          id="uploadText"
+          maxLength={1000}
+          cols={30}
+          rows={10}
+          value={text}
           onChange={(e) => {
-            setTitle(e.target.value);
+            setText(e.target.value);
           }}
-        ></Input>
-        <form className="uploadInfo">
-          <textarea
-            id="uploadText"
-            maxLength={1000}
-            cols={30}
-            rows={10}
-            value={text}
-            onChange={(e) => {
-              setText(e.target.value);
-            }}
-            placeholder="문구를 입력해주세요..."
-          ></textarea>
-        </form>
-      </div>
-      <Button type="button" onClick={handleSubmit}>
-        업로드
+          placeholder="묵상한 내용을 적어주세요"
+          className="w-[100%] h-[120px] px-3 py-2 text-sm placeholder:text-muted-foreground focus-visible:outline-none focus:ring-violet-300   disabled:cursor-not-allowed disabled:opacity-50 border-2 border-gray-300 rounded-sm"
+        ></textarea>
+      </form>
+
+      <Button type="button" onClick={handleSubmit} className="mt-[15px]">
+        Upload
       </Button>
-      <Button onClick={() => closeUploadModal()}>닫기</Button>
+
+      <img
+        src={closeImg}
+        alt="업로드 모달창 닫기"
+        onClick={() => closeUploadModal()}
+        className="cursor-pointer absolute top-4 right-4"
+      />
     </dialog>
   );
 }
