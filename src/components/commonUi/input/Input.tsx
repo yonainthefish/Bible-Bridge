cd08829241f7d7aeb3427f836bc03cd3e0756ee8
yonainthefish <yonaisgood@gmail.com>
2024-02-22 01:50:09 +0900

    :art: Design: Input 스타일 수정(#6)

--- src/components/commonUi/input/Input.tsx
+++ src/components/commonUi/input/Input.tsx
@@ -11,7 +11,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          'flex h-10 w-full bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus:ring-violet-300   disabled:cursor-not-allowed disabled:opacity-50',
+          'h-10 w-full bg-background px-3 py-2 mb-5 text-sm flex border border-gray-200 rounded-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus:ring-violet-300   disabled:cursor-not-allowed disabled:opacity-50',
           className,
         )}
         ref={ref}
