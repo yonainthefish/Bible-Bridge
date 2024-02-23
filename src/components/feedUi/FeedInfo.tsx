55cef6cad5172c8983d07d413f55b42a98c573fc
yonainthefish <yonaisgood@gmail.com>
2024-02-23 15:26:36 +0900

    :recycle: Refactor: UserImgAndName 컴포넌트 적용하여 리팩토링(#34)

--- src/components/feedUi/FeedInfo.tsx
+++ src/components/feedUi/FeedInfo.tsx
@@ -7,8 +7,8 @@ import { FeedAndUserInfo } from '@/components/feedUi/model';
 import DateFormatter from '@/components/commonUi/date/DateFomatter';
 import Overlay from '@/components/commonUi/overlay/Overlay';
 import Liked from '@/components/userReactionUi/likeUi/Liked';
+import UserImgAndName from '@/components/profileUi/UserImgAndName';
 
-import BasicUserImg from '@/assets/Img/Img-user.svg';
 import Calendar from '@/assets/Icon/Icon-calendar.svg';
 
 interface FeedItemProps {
@@ -21,22 +21,14 @@ const FeedInfo: React.FC<FeedItemProps> = ({ feed }) => {
   const handleLinkClick = () => {
     setPrevPath(window.location.pathname);
   };
-  console.log(feed);
+
   return (
     <Link to={`/feed/${feed.id}`} onClick={handleLinkClick}>
       <div className="w-[100%] bg-white ">
-        <div className="flex justify-between my-1 px-2">
-          <div className="flex items-center gap-2">
-            <img
-              src={feed.authorPhotoURL || BasicUserImg}
-              alt="User"
-              className="h-9 w-9 rounded-full object-cover"
-            />
-            <p className="text-small">
-              {feed.authorDisplayName || '알 수 없음'}
-            </p>
-          </div>
-        </div>
+        <UserImgAndName
+          authorPhotoURL={feed.authorPhotoURL}
+          authorDisplayName={feed.authorDisplayName}
+        />
 
         <section className="aspect-square border-2 rounded-lg overflow-hidden relative">
           {feed.imageUrl && (
@@ -60,7 +52,6 @@ const FeedInfo: React.FC<FeedItemProps> = ({ feed }) => {
 
         <div className="flex gap-1 text-gray-500 text-sm">
           <img src={Calendar} alt="달력" className="text-gray-700" />
-          {/* {formatDate(feed.timestamp)} */}
           <DateFormatter date={feed.timestamp.toDate()} />
         </div>
       </div>
