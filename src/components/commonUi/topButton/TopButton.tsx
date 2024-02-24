41a417d29675edc97ca46c6205b1124d07e715e0
yonainthefish <yonaisgood@gmail.com>
2024-02-24 16:59:40 +0900

    :art: Design: 공통컴포넌트(탑바) 스타일 수정(#6)

--- src/components/commonUi/topButton/TopButton.tsx
+++ src/components/commonUi/topButton/TopButton.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 
 import '@/components/commonUi/topButton/TopButton.css';
 
-import ArrowTop from '@/assets/Icon/Icon-up.svg';
+import ArrowTop from '@/assets/Icon/Icon-upBtn.svg';
 
 export default function TopButton() {
   const [ScrollY, setScrollY] = useState(0);
@@ -11,22 +11,19 @@ export default function TopButton() {
   const handleFollow = () => {
     setScrollY(window.pageYOffset);
     if (ScrollY > 100) {
-      // 100 이상이면 버튼이 보이게
       setBtnStatus(true);
     } else {
-      // 100 이하면 버튼이 사라지게
       setBtnStatus(false);
     }
   };
 
   const handleTop = () => {
-    // 클릭하면 스크롤이 위로 올라가는 함수
     window.scrollTo({
       top: 0,
       behavior: 'smooth',
     });
-    setScrollY(0); // ScrollY 의 값을 초기화
-    setBtnStatus(false); // BtnStatus의 값을 false로 바꿈 => 버튼 숨김
+    setScrollY(0);
+    setBtnStatus(false);
   };
 
   useEffect(() => {
@@ -44,7 +41,7 @@ export default function TopButton() {
       className={BtnStatus ? 'topBtn active' : 'topBtn'}
       onClick={handleTop}
     >
-      <img src={ArrowTop} alt="최상단으로 가기" />
+      <img src={ArrowTop} alt="최상단으로 가기" className="w-[18px]" />
     </button>
   );
 }
