ffe4043d1a5ca8051b1907875269ae60b97567dc
yonainthefish <yonaisgood@gmail.com>
2024-02-24 17:00:23 +0900

    :art: Design: 공통컴포넌트(헤더) 스타일 수정(#6)

--- src/components/commonUi/header/Header.tsx
+++ src/components/commonUi/header/Header.tsx
@@ -1,10 +1,33 @@
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+
 import Logo from '@/assets/Img/Logo-text-small.svg';
 
 export default function Header() {
+  const [shadow, setShadow] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 10) {
+        setShadow(true);
+      } else {
+        setShadow(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <>
-      <header className="w-[calc(100%-80px)] h-20 px-16 bg-gray-0 flex items-center fixed top-0 left-0 right-0 z-20">
+      <header
+        className={`w-[calc(100%-80px)] h-20 px-16 bg-gray-0 flex items-center fixed top-0 left-0 right-0 z-20
+        ${shadow ? 'shadow-md' : ''}`}
+      >
         <Link to="/home">
           <h1>
             <img className="w-[250px]" src={Logo} alt="바이블브릿지 로고" />
