0ae1b76d0c5040d51a0a5c6a2a89805f442086ef
yonainthefish <yonaisgood@gmail.com>
2024-03-06 23:21:57 +0900

    :art: Design: 스플래시화면 디자인 수정()

--- src/pages/splash/StaticSplash.tsx
+++ src/pages/splash/StaticSplash.tsx
@@ -1,3 +1,9 @@
+import LogoImg from '@/assets/Img/Logo-big.svg';
+
 export default function StaticSplash() {
-  return <p>StaticSplash</p>;
+  return (
+    <div className="h-[100vh] flex justify-center items-center">
+      <img src={LogoImg} alt="바이블브릿지로고" />
+    </div>
+  );
 }
